03013356a433d0e6d3c8fedd47fb984710aa7f2f
catdog50rus <catdog50rus@gmail.com>
2019-12-02 17:45:03 +0300

    02.12.19

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,18 @@
 $(function(){
 
-
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrow__left" src = "images/arrow-left.svg" alt = "">',
+        nextArrow: '<img class="slider-arrows slider-arrow__right" src = "images/arrow-right.svg" alt = "">',
+        asNavFor: '.slider-dots',
+    });
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+        focusOnSelect: true
+    });
 
   
 });
