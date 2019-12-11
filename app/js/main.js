5c50b0cc1b7577960168470e7e55946fac2f503a
catdog50rus <catdog50rus@gmail.com>
2019-12-11 12:54:06 +0300

    11.12.19

--- app/js/main.js
+++ app/js/main.js
@@ -36,51 +36,61 @@ $(function(){
         slidesToScroll: 1,
         prevArrow: '<img class="slider-arrows slider-arrow__left" src = "images/arrow-left-dark.svg" alt = "">',
         nextArrow: '<img class="slider-arrows slider-arrow__right" src = "images/arrow-right-dark.svg" alt = "">',
+        focusOnSelect: false,
+        
     });
 
-
-    const guestPlus = document.querySelector('.guest__plus');
-    const guestMinus = document.querySelector('.guest__minus');
-    const nightPlus = document.querySelector('.night__plus');
-    const nightMinus = document.querySelector('.night__minus');
+    const guestPlus = $('.guest__plus');
+    const guestMinus = $('.guest__minus');
+    const nightPlus = $('.night__plus');
+    const nightMinus = $('.night__minus');
     let guest = 1;
     let night = 1;
     let totalPrice = 0;
-    let titleNumNight = document.querySelector('.title_night-number');
-    let titleNumGuest = document.querySelector('.title_guest-number');
-    let titleTotalPrice = document.querySelector('.title_total-price');
-    let spanPerNight = document.querySelector('.per-night');
-    
+    let titleNumNight = $('.title_night-number');
+    let titleNumGuest = $('.title_guest-number');
+    let titleTotalPrice = $('.title_total-price');
+    let spanPerNight = $('.per-night');
     print();
 
-    guestMinus.addEventListener('click', () => {
+    guestMinus.click(function () {
         guest--;
-        if(guest < 0) {
+        if (guest < 0) {
             guest = 0;
         }
-        
-        print();
-    });
 
-    guestPlus.addEventListener('click', () => {
+        print();
+        console.log(guest);
+
+    });
+    
+    guestPlus.click(function(){
         guest++;
+
         print();
-        
+        console.log(guest);
+
     });
 
-    nightMinus.addEventListener('click', () => {
+    nightMinus.click(function () {
         night--;
         if (night < 0) {
             night = 0;
         }
 
         print();
+
+        console.log(night);
     });
 
-    nightPlus.addEventListener('click', () => {
+    nightPlus.click(function () {
         night++;
-        
+
         print();
+
+        console.log(night);
+
+        
 
     });
 
@@ -88,40 +98,50 @@ $(function(){
         $('.guest-number').text(guest + ' ');
         $('.night-number').text(night + ' ');
         $('.total-price').text(getTotalPrice() + ' ');
-        if(night >= 10){
-            
-            titleNumNight.style.width='265px';
-        }
-        else{
-            titleNumNight.style.width = '242px';
+        
+        if (night >= 10) {
+
+            titleNumNight.css({'width':"265px"});
+        } else {
+            titleNumNight.css({'width':"242px"});
         }
 
         if (guest >= 10) {
 
-            titleNumGuest.style.width = '270px';
+            titleNumGuest.css({'width':"270px"});
         } else {
-            titleNumGuest.style.width = '242px';
+            titleNumGuest.css({'width':"242px"});
         }
 
-        if (totalPrice >= 100) {
-
-            titleTotalPrice.style.width = '300px';
-            spanPerNight.style = "margin-right:10px;";
-            //spanPerNight.style="text-aline:none;"
-        }
-        if (totalPrice >= 1000) {
-
-            titleTotalPrice.style.width = '300px';
-            spanPerNight.style="margin-right:10px;";
-            //spanPerNight.style="text-aline:none;"
-        }
         
+        if (totalPrice >= 100 && totalPrice < 1000) {
+
+            titleTotalPrice.css({'width':'270px'});
+            spanPerNight.css({'margin-right':'12px'});
+        }
+        if (totalPrice >= 1000 && totalPrice < 10000) {
+
+            titleTotalPrice.css({'width':'300px'});
+            spanPerNight.css({'margin-right':'13px'});
+        }
+        if (totalPrice >= 10000 && totalPrice < 100000) {
+
+            titleTotalPrice.css({'width':'330px'});
+            spanPerNight.css({'margin-right':'16px'});
+        }
+        if (totalPrice >= 100000) {
+
+            titleTotalPrice.css({'width':'360px'});
+            spanPerNight.css({'margin-right':'16px'});
+        }
+
     }
-    
-    function getTotalPrice () {
+
+    function getTotalPrice() {
         totalPrice = 0;
-        totalPrice = (night * 82)*guest;
+        totalPrice = (night * 82) * guest;
         return totalPrice;
     };
+    
   
 });
